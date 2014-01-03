db3ce25c9a5ff784af38592c87ca349fddd2a404
bunel <bunel.arnaud@gmail.com>
2014-01-04 00:07:27 +0100

    cases

--- orthotab/src/main/webapp/app/js/controllers.js
+++ orthotab/src/main/webapp/app/js/controllers.js
@@ -5,20 +5,11 @@
 var orthotabControllers = angular.module('orthotabControllers', []);
 
   orthotabControllers.controller('CouleursCtrl', ['$rootScope','$scope', 'Couleurs',
-  function($rootScope, $scope, Couleurs) {
-	//$rootScope.boutonCouleurClass = 'boutonCouleur';
-	 // $scope.boutonCouleurClass = 'boutonCouleur';
-	  
-	  var self = this;
-	  self.selectedIndex = -1;
+  function($rootScope, $scope, Couleurs) {	  
+	var self = this;
+	self.selectedIndex = -1;
+	
     $scope.couleurs = Couleurs.query();
-    //$scope.boutonCouleur-style = "boutonCouleur";
-    //$scope.orderProp = 'age';
-    
-   /* $scope.logPosition = function (item, $event) {
-    	console.log(item + ' was clicked at: ' + $event.clientX + ',' +
-    	$event.clientY);
-    	}*/
     
     $scope.getClass = function(ind){
         if( ind === self.selectedIndex ){
@@ -29,18 +20,61 @@ var orthotabControllers = angular.module('orthotabControllers', []);
     }
     
     $scope.enregistreCouleur = function (couleur, $event, ind) {
-		console.log("debut");
 		self.selectedIndex = ind;
 		$rootScope.codeCouleurCourant = couleur.code;
+		$rootScope.messageNiveau01 = "";
 		console.log("codeCouleurCourant : " + $rootScope.codeCouleurCourant);
 		}
     
   }]);
   
-  orthotabControllers.controller('Niveau01Ctrl', ['$scope', 'Niveau01',
-                                                  function($scope, Niveau01) {
-                                                    $scope.cases = Niveau01.query();
-                                                  }]);
+  orthotabControllers.controller('Niveau01Ctrl', ['$rootScope','$scope', 'Niveau01',
+  function($rootScope, $scope, Niveau01) {
+		var self = this;
+		self.selectedIndex = -1;
+	  $scope.cases = Niveau01.query();
+                                                    
+                                                    
+	    $scope.getClass = function(ind){
+	        if( ind === self.selectedIndex){
+	            return "boutonCaseActif";
+	        } else{
+	            return "boutonCase";
+	        }
+	    }
+	    
+	    $scope.getBackgrdStyle = function(ind){
+	        if( ind === self.selectedIndex && $rootScope.codeCouleurCourant!=null){
+	            return $rootScope.codeCouleurCourant;
+	        } else{
+	        	return "#B5B276";
+	        }
+	    }
+	    $scope.enregistreCase = function (caseCombi, $event, ind) {
+			//console.log("debut");
+	    	console.log("codeCouleurCourant : " + $rootScope.codeCouleurCourant);
+	    	if($rootScope.codeCouleurCourant==null){
+	    		$rootScope.messageNiveau01 = "Il faut d'abord selectionner une case de couleur";
+	    	}else if($rootScope.caseCombiCourant==null){
+	    		self.selectedIndex = ind;
+	    		$rootScope.caseCombiCourant = caseCombi;
+	    		$rootScope.messageNiveau01 = "";
+	    	}else{
+	    		var typeCaseFirst = $rootScope.caseCombiCourant.type;
+	    		console.log("typeCaseFirst : " + typeCaseFirst);
+	    		var typeCaseSecond = caseCombi.type;
+	    		console.log("typeCaseSecond : " + typeCaseSecond);
+	    		if($rootScope.caseCombiCourant.valeur == caseCombi.valeur){
+	    			console.log("correspondance Ok");
+	    			$rootScope.correspondanceOK = true;
+	    			//self.class="boutonCaseActif";
+	    		}
+	    	}
+			
+			//$rootScope.codeCouleurCourant = couleur.code;
+			//console.log("codeCouleurCourant : " + $rootScope.codeCouleurCourant);
+			}                       
+  }]);
 
 /*phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
   function($scope, $routeParams, Phone) {
